1df2b8c1413bc91ecc570e3e84387c80815a0306
MuhammadSodiq <sening-email@gmail.com>
2025-06-10 19:15:01 +0300

    Windowsdan Macbookga

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer'
 
 const App = () => {
     return (
-        <div>
+        <div style={{ background: '#F2F7FB' }}>
             <Header />
             <Body />
             <Footer />
